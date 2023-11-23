825d71913208efbb8c1006e3daaacf580733cea7
XMars <truong.pham2@sotatek.com>
2023-11-23 09:35:36 +0700

    update

--- .cicd/scripts/utils.sh
+++ .cicd/scripts/utils.sh
@@ -13,7 +13,7 @@ get_cicd_config_for_odoo_addon() {
 
 docker_compose() {
     cd $ODOO_DOCKER_COMPOSE_PATH
-    docker compose -p "$ODOO_DOCKER_COMPOSE_PROJECT_NAME" "$@"
+    docker compose -p "$@"
 }
 
 docker_compose_clean() {
