e2dd6b15aa06b4ee120a73e9d4f27c4629b4774b
XMars <truong.pham2@sotatek.com>
2023-11-24 10:42:12 +0700

    update

--- pos_discount_all/tests/test_module.py
+++ pos_discount_all/tests/test_module.py
@@ -42,7 +42,7 @@ class TestUi(TestPointOfSaleHttpCommon):
                 "name": "Generic Product",
                 "available_in_pos": True,
                 "list_price": 10.0,
-                "taxes_id12": False,
+                "taxes_id": False,
             }
         )
 
